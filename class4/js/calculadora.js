e77214360eef6c074743e81c2b422e991d82f036
Kevin Maximiliano Palma Romero <kev.mpr03@gmail.com>
2024-06-16 19:34:47 -0300

    Correccion de nombres en las funciones, declaración de variables e intento de reutilizacion del codigo

--- class4/js/calculadora.js
+++ class4/js/calculadora.js
@@ -1,54 +1,60 @@
-let operacion = parseInt(prompt('OPERACIONES \n1 - Suma \n2 - Resta \n3 - Multiplicación \n4 - División'));
+let k = 0;
+while (k == 0){
+    const opcion = parseInt(prompt('OPERACIONES \n1 - Suma \n2 - Resta \n3 - Multiplicación \n4 - División \n5 - Salir'));
 
-switch (operacion){
-    case 1:
-        Sumar();
-        break;
-    case 2:
-        Restar()
-        break;
-    case 3:
-        Multiplicar();
-        break;
-    case 4:
-        Dividir();
-        break;
-    default:
-        alert('Ingrese un numero valido')
+    switch (opcion){
+        case 1:
+            sumar();
+            break;
+        case 2:
+            restar()
+            break;
+        case 3:
+            multiplicar();
+            break;
+        case 4:
+            dividir();
+            break;
+        case 5:
+            k = 1;
+            break;
+        default:
+            alert('Ingrese un numero valido')
+    }
 }
 
-function Sumar(){
-    let num1 = parseInt(prompt('Escriba el primer numero a sumar'));
-    let num2 = parseInt(prompt('Escriba el segundo numero a sumar'))
 
-    let resultado = num1 + num2;
+function obtenerNumeros(operacion){
+    const num1 = parseInt(prompt(`Escriba el primer numero a ${operacion}`));
+    const num2 = parseInt(prompt(`Escriba el segundo numero a ${operacion}`));
+    
+    return [num1, num2];
+}
+
+function sumar(){
+    const [num1, num2] = obtenerNumeros('sumar')
+    const resultado = num1 + num2;
 
     return alert(`${num1} + ${num2} = ${resultado}`)
 }
 
-function Restar(){
-    let num1 = parseInt(prompt('Escriba el primer numero a restar'));
-    let num2 = parseInt(prompt('Escriba el segundo numero a restar'))
-
-    let resultado = num1 - num2;
+function restar(){
+    const [num1, num2] = obtenerNumeros('restar')
+    const resultado = num1 - num2;
 
     return alert(`${num1} - ${num2} = ${resultado}`)
 }
 
-function Multiplicar(){
-    let num1 = parseInt(prompt('Escriba el primer numero a multiplicar'));
-    let num2 = parseInt(prompt('Escriba el segundo numero a multiplicar'))
-
-    let resultado = num1 * num2;
+function multiplicar(){
+    const [num1, num2] = obtenerNumeros('multiplicar')
+    const resultado = num1 * num2;
 
     return alert(`${num1} * ${num2} = ${resultado}`)
 }
 
-function Dividir(){
-    let num1 = parseInt(prompt('Escriba el primer numero a dividir'));
-    let num2 = parseInt(prompt('Escriba el segundo numero a dividir'))
-
-    let resultado = num1 / num2;
+function dividir(){
+    const [num1, num2] = obtenerNumeros('dividir')
+    const resultado = num1 / num2;
 
     return alert(`${num1} / ${num2} = ${resultado}`)
 }
