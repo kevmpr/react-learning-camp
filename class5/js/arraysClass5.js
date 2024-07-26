8b4ee914c8dd0a1be9258fb0a028c58dd0cc7866
Kevin Maximiliano Palma Romero <kev.mpr03@gmail.com>
2024-07-25 21:04:39 -0300

    Playing with objects and arrays

---
+++ class5/js/arraysClass5.js
@@ -0,0 +1,15 @@
+const fruits = ['Banana', 'Apple', 'Pineapple']
+alert(`My fruits are: ${fruits}`);
+
+fruits.unshift('Orange');
+fruits.push('Peach');
+fruits.sort();
+
+alert(`My new ordered fruits are: ${fruits}`);
+
+fruits.shift();
+fruits.pop();
+
+alert(`My new few ordered fruits are: ${fruits}`);
+
+
